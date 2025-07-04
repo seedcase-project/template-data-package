0bf1d1066e7ffa04fb7d07744319198d4487b711
martonvago <marci.vago@gmail.com>
2025-07-04 15:47:05 +0100

    feat: :sparkles: fill in main.py based on guides

--- main.py
+++ main.py
@@ -1,15 +1,61 @@
+# import polars as pl
 import seedcase_sprout as sp
 
-# from scripts.properties import properties
+from scripts.properties import properties
+
+# from scripts.resource_properties import resource_properties
 
 
 def main():
     """Run the build pipeline of the data package."""
-    # Create the properties script in default location.
+    ## PROPERTIES
+
+    ## Create the properties script in default location if it doesn't already exist.
+    package_path = sp.PackagePath()
     sp.create_properties_script()
 
-    # Write properties from properties script to `datapackage.json`.
-    # sp.write_properties(properties=properties)
+    ## Load your raw, but tidy, data into a Polars DataFrame.
+    # raw_data = pl.read_csv(package_path.root() / "raw" / "data.csv")
+    ## Extract field properties from the data.
+    # field_properties = sp.extract_field_properties(data=raw_data)
+
+    ## Create the resource properties script if it doesn't already exist.
+    # sp.create_resource_properties_script(
+    #     resource_name="",
+    #     fields=field_properties,
+    # )
+
+    ## Save the properties to `datapackage.json`.
+    sp.write_properties(properties=properties)
+
+    ## README
+
+    ## Create the README text for the data package.
+    readme_text = sp.as_readme_text(properties)
+    ## Write the README text to a `README.md` file.
+    sp.write_file(readme_text, package_path.readme())
+
+    ## BATCH DATA
+
+    ## Save the batch data.
+    # sp.write_resource_batch(
+    #     data=raw_data, resource_properties=resource_properties
+    # )
+
+    ## RESOURCE DATA
+
+    ## Read in all the batch data files for the resource as a list.
+    # batch_data = sp.read_resource_batches(
+    #     resource_properties=resource_properties
+    # )
+    ## Join them all together into a single Polars DataFrame.
+    # joined_data = sp.join_resource_batches(
+    #     data_list=batch_data, resource_properties=resource_properties
+    # )
+    ## Write the resource data file.
+    # sp.write_resource_data(
+    #     data=joined_data, resource_properties=resource_properties
+    # )
 
 
 if __name__ == "__main__":
