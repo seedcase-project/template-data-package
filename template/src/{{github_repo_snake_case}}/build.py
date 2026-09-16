050e6e60f538fb5345059d8baf84cccff4d0189c
Luke W. Johnston <lwjohnst@gmail.com>
2026-09-16 10:11:59 +0200

    fix: 🚨 add docstrings and return to `build.py`

--- template/src/{{github_repo_snake_case}}/build.py
+++ template/src/{{github_repo_snake_case}}/build.py
@@ -4,12 +4,18 @@ import pytask
 
 
 @pytask.mark.metadata
-def task_metadata_STEP(): ...
+def task_metadata_STEP():
+    """Task for metadata."""
+    return
 
 
 @pytask.mark.raw
-def task_download_raw_STEP(): ...
+def task_download_raw_STEP():
+    """Task for downloading raw from PLACE."""
+    return
 
 
 @pytask.mark.staging
-def task_process_STEP(): ...
+def task_process_STEP():
+    """Task for processing data for STEP."""
+    return
